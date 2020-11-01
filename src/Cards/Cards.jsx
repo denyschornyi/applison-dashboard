19b166b03cf077497c2df0e38e6bc827cc6dfefe
Denys Chornyi <chornijj14@gmail.com>
2020-11-01 22:47:14 +0100

    Cards basic structure done

--- src/Cards/Cards.jsx
+++ src/Cards/Cards.jsx
@@ -4,7 +4,16 @@ function Cards() {
   return (
     <div className="cards">
       <div className="row">
-        <div className="col-lg-3 col-md-6">
+        <div className="col-md-3">
+          <div className="card">
+            <h2 className="card__infoNumber">$ 278</h2>
+            <div>
+              <span className="card__title">Total Profit</span>
+              <span className="card__growth growth__positive">+4.5%</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
           <div className="card">
             <h2 className="card__infoNumber">278</h2>
             <div>
@@ -13,25 +22,16 @@ function Cards() {
             </div>
           </div>
         </div>
-        <div className="col-lg-3 col-md-6">
+        <div className="col-md-3">
           <div className="card">
             <h2 className="card__infoNumber">278</h2>
             <div>
               <span className="card__title">Total Profit</span>
-              <span className="card__growth growth__positive">+4.5%</span>
+              <span className="card__growth growth__negative">+4.5%</span>
             </div>
           </div>
         </div>
-        <div className="col-lg-3 col-md-6">
-          <div className="card">
-            <h2 className="card__infoNumber">278</h2>
-            <div>
-              <span className="card__title">Total Profit</span>
-              <span className="card__growth growth__positive">+4.5%</span>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6">
+        <div className="col-md-3">
           <div className="card">
             <h2 className="card__infoNumber">278</h2>
             <div>
