afc6d5e4852b6892da910069a874adb49df0701d
Denys Chornyi <chornijj14@gmail.com>
2020-11-02 11:14:12 +0100

    Add material UI design and make a select

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./Header.css";
 
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+
 function Header() {
   return (
     <div className="header">
       <div className="row">
-        <div className="col-md-12">
+        <div className="col-md-6">
           <h1>Enterprise Shiny Dashboard</h1>
+        </div>
+        <div className="col-md-6 text-lg-right text-md-left">
+          <FormControl className="app__dropdown">
+            <Select variant="outlined">
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+            </Select>
+          </FormControl>
         </div>
       </div>
     </div>
