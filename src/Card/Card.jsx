e83048be14c2f037fd6cb865cab9ac8e3559f384
Denys Chornyi <chornijj14@gmail.com>
2020-11-02 13:00:37 +0100

    Check props types value via PropTypes

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Card.css";
+import PropTypes from "prop-types";
 
-function Card({ cases, title, growth }) {
+function Card({ cases, title, total }) {
   return (
     <div className="card">
       <h2 className="card__infoNumber">{cases}</h2>
-
-      <span className="card__title">{title}</span>
+      <p className="card__title">{title}</p>
+      <p>{total} Total</p>
     </div>
   );
 }
+
+Card.propTypes = {
+  cases: PropTypes.number,
+  title: PropTypes.string,
+  total: PropTypes.number
+};
 
 export default Card;
--- src/Cards/Cards.jsx
+++ src/Cards/Cards.jsx
@@ -9,13 +9,25 @@ function Cards({ countryInfo }) {
     <div className="cards">
       <div className="row">
         <div className="col-md-4 col-sm-4 col-12">
-          <Card cases={countryInfo.todayCases} title="Coronavirus cases" />
+          <Card
+            cases={countryInfo.todayCases}
+            title="Coronavirus cases"
+            total={countryInfo.cases}
+          />
         </div>
         <div className="col-md-4 col-sm-4 col-12">
-          <Card cases={countryInfo.todayRecovered} title="Recovered" />
+          <Card
+            cases={countryInfo.todayRecovered}
+            title="Recovered"
+            total={countryInfo.recovered}
+          />
         </div>
         <div className="col-md-4 col-sm-4 col-12">
-          <Card cases={countryInfo.todayDeaths} title="Death" />
+          <Card
+            cases={countryInfo.todayDeaths}
+            title="Death"
+            total={countryInfo.deaths}
+          />
         </div>
       </div>
     </div>
