1625d13e35a49c4fca7ba4b9ad9cae22ecc70070
Denys Chornyi <chornijj14@gmail.com>
2020-11-02 22:00:47 +0100

    Google marks warning fixed

--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -1,31 +1,26 @@
 import React from "react";
 import "./Map.css";
 import GoogleMapReact from "google-map-react";
-import MapMarker from "google-map-react";
 import numeral from "numeral";
 
-function Map({ countries, mapCenter, mapZoom }) {
-  const Countries = ({ countries }) => {
-    return countries.map((country) => (
-      <div
-        key={country.country}
-        className="info-container"
-        lat={country.countryInfo.lat}
-        lng={country.countryInfo.long}
-        style={{
-          color: "red",
-          backgroundColor: "#fff",
-          height: "25px",
-          width: "50px",
-          textAlign: "center"
-        }}
-      >
-        <img height="10px" src={country.countryInfo.flag} />
-        <p>{numeral(country.cases).format("0,0")}</p>
-      </div>
-    ));
-  };
+import Card from "@material-ui/core/Card";
 
+function Map({ countries, mapCenter, mapZoom }) {
+  const CountryMarker = ({ country }) => (
+    <Card
+      className="info-container"
+      style={{
+        color: "red",
+        backgroundColor: "#fff",
+        height: "25px",
+        width: "50px",
+        textAlign: "center"
+      }}
+    >
+      <img height="10px" src={country.countryInfo.flag} />
+      <p>{numeral(country.cases).format("0,0")}</p>
+    </Card>
+  );
   return (
     <div style={{ height: "500px", width: "100%" }} className="map">
       <h2 className="text-center map__title">Worldwide cases by country</h2>
@@ -37,7 +32,14 @@ function Map({ countries, mapCenter, mapZoom }) {
         center={mapCenter ? mapCenter : ""}
         zoom={mapZoom ? mapZoom : ""}
       >
-        <Countries countries={countries} />
+        {countries.map((country) => (
+          <CountryMarker
+            key={country.country}
+            lat={country.countryInfo.lat}
+            lng={country.countryInfo.long}
+            country={country}
+          />
+        ))}
       </GoogleMapReact>
     </div>
   );
