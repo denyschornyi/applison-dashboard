956cc85cd1fd2ed0432e6203bbfb8ab82b67a211
Denys Chornyi <chornijj14@gmail.com>
2020-11-02 19:08:15 +0100

    Clean Google Map warnings and errors

--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 import "./Map.css";
 import GoogleMapReact from "google-map-react";
+import MapMarker from "google-map-react";
 import numeral from "numeral";
 
 function Map({ countries, mapCenter, mapZoom }) {
-  console.log(countries);
-  const countriesLocation = countries.map((country) => (
-    <div
-      className="info-container"
-      lat={country.countryInfo.lat}
-      lng={country.countryInfo.long}
-      style={{
-        color: "red",
-        backgroundColor: "#fff",
-        height: "25px",
-        width: "50px",
-        textAlign: "center"
-      }}
-    >
-      <img height="10px" src={country.countryInfo.flag} />
-      <p>{numeral(country.cases).format("0,0")}</p>
-    </div>
-  ));
+  const Countries = ({ countries }) => {
+    return countries.map((country) => (
+      <div
+        key={country.country}
+        className="info-container"
+        lat={country.countryInfo.lat}
+        lng={country.countryInfo.long}
+        style={{
+          color: "red",
+          backgroundColor: "#fff",
+          height: "25px",
+          width: "50px",
+          textAlign: "center"
+        }}
+      >
+        <img height="10px" src={country.countryInfo.flag} />
+        <p>{numeral(country.cases).format("0,0")}</p>
+      </div>
+    ));
+  };
 
   return (
     <div style={{ height: "500px", width: "100%" }} className="map">
@@ -29,12 +32,12 @@ function Map({ countries, mapCenter, mapZoom }) {
 
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyAEIVSOu4JZtqLCs6X1no1b0STscyIqpHs" }}
-        defaultCenter={mapCenter}
-        defaultZoom={mapZoom}
-        center={mapCenter}
-        zoom={mapZoom}
+        defaultCenter={{ lat: 34.80746, lng: -40.4796 }}
+        defaultZoom={3}
+        center={mapCenter ? mapCenter : ""}
+        zoom={mapZoom ? mapZoom : ""}
       >
-        {countriesLocation}
+        <Countries countries={countries} />
       </GoogleMapReact>
     </div>
   );
